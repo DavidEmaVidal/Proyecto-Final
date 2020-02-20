8acf7e5212854816786616ebfde588c00a8522d8
DavidEmaVidal <DavidEmaVidal@users.noreply.github.com>
2020-02-20 10:15:22 -0300

    Entidad Usuario

---
+++ src/main/java/com/example/demo/TomaMiManoWeb/Entidades/Usuario.java
@@ -0,0 +1,70 @@
+package com.example.demo.TomaMiManoWeb.Entidades;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
+
+@Entity
+public class Usuario {
+    @Id
+    private String id_usu;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    @Temporal(TemporalType.DATE)
+    private Date fecha_nac;
+    private Integer creditos;
+
+    public Usuario() {
+    }
+
+    public String getId_usu() {
+        return id_usu;
+    }
+
+    public void setId_usu(String id_usu) {
+        this.id_usu = id_usu;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(Date fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public Integer getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(Integer creditos) {
+        this.creditos = creditos;
+    }
+}
