aad4483a421e67b557db9b268a372f9dffc63a94
Ricardo <scaraffiarj@gmail.com>
2020-02-20 10:52:39 -0300

    entidad foto

---
+++ src/main/java/com/example/demo/TomaMiManoWeb/Entidades/Foto.java
@@ -0,0 +1,60 @@
+package com.example.demo.TomaMiManoWeb.Entidades;
+
+import javax.persistence.Basic;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+
+@Entity
+public class Foto {
+	
+	@Id
+	private String id_foto;
+	private String nombre;
+	private String mime;
+	@Lob @Basic(fetch = FetchType.LAZY) //tipo de dato grande
+	private byte[] contenido;
+	
+	public Foto() {
+		super();
+	}
+
+	public String getId_foto() {
+		return id_foto;
+	}
+
+	public void setId_foto(String id_foto) {
+		this.id_foto = id_foto;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMime() {
+		return mime;
+	}
+
+	public void setMime(String mime) {
+		this.mime = mime;
+	}
+
+	public byte[] getContenido() {
+		return contenido;
+	}
+
+	public void setContenido(byte[] contenido) {
+		this.contenido = contenido;
+	}
+	
+	
+	
+	
+	
+	
+}
